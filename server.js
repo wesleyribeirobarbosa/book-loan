188a755270392444b19b5c6ce95da33c8325c153
Wesley Ribeiro Barbosa <wesleysrs_95@hotmail.com>
2021-03-06 00:54:19 -0300

    Create routes and controller files (for User and Book entities only)

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-require('dotenv').config()
-const express = require("express")
-var app = express()
+require('dotenv').config();
+const express = require("express");
+const app = express();
+var router = require('./src/routes/Routes')
 
 app.use(express.json())
+app.use('/', router)
 app.listen(process.env.PORT, () => console.log("Servidor iniciado na porta", process.env.PORT))
---
+++ src/api/controller/User.js
@@ -0,0 +1,17 @@
+// var User = require('../model/User');
+
+exports.save = (req,res) => {
+    res.send('NOT IMPLEMENTED: save');
+};
+
+exports.retrieve = (req,res) => {
+    res.send('NOT IMPLEMENTED: retrieve');
+};
+
+exports.edit = (req,res) => {
+    res.send('NOT IMPLEMENTED: edit');
+};
+
+exports.remove = (req,res) => {
+    res.send('NOT IMPLEMENTED: remove');
+};
